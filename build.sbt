972c79a84f4c18d4f9728053ef3bfafa4b0bc17d
song1279 <1183874219@qq.com>
2018-09-26 17:03:33 +0800

    demo

--- build.sbt
+++ build.sbt
@@ -17,3 +17,7 @@ libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.3.1"
 libraryDependencies += "org.apache.spark" %% "spark-hive" % "2.3.1"
 
 libraryDependencies += "org.apache.spark" %% "spark-graphx" % "2.3.1"
+
+libraryDependencies += "junit" % "junit" % "4.12"
+
+libraryDependencies += "org.apache.kafka" % "kafka-clients" % "2.0.0"
--- src/main/java/kafka/KafkaProvider.java
+++ src/main/java/kafka/KafkaProvider.java
@@ -1,35 +1,36 @@
 package kafka;
 
 
-
-
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+
 
 import java.util.Properties;
 import java.util.concurrent.ExecutionException;
 
 public class KafkaProvider {
 
-    public static void setProducer(){
+
+
+    public static void setProducer() {
         Properties kafkaProps = new Properties();
-        kafkaProps.setProperty("bootstrap.servers","PLAINTEXT://192.168.1.100:9092");
+        kafkaProps.setProperty("bootstrap.servers", "PLAINTEXT://192.168.1.100:9092");
         kafkaProps.setProperty("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         kafkaProps.setProperty("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         KafkaProducer<String, String> producer = new KafkaProducer<String, String>(kafkaProps);
 
+
         ProducerRecord<String, String> record =
-                new ProducerRecord<String, String>("test","Precision products", "France");
+                new ProducerRecord<String, String>("test", "Precision products", "mother fucker");
 
         try {
-            producer.send(record).get();
+            RecordMetadata a = producer.send(record).get();
         } catch (InterruptedException e) {
             e.printStackTrace();
         } catch (ExecutionException e) {
             e.printStackTrace();
         }
-
-
     }
 
 
